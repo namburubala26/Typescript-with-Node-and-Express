696d38534fac5ecf209b492166a83ca90785980c
namburubala26 <115918201+namburubala26@users.noreply.github.com>
2023-06-21 11:04:39 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,8 @@
+// import express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import todoRoutes from './routes/todos';
+const app = express();
+app.use(bodyParser.json());
+app.use(todoRoutes);
+app.listen({port:3000});
